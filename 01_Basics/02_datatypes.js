be65530cd3aacbf1a592024e2856f5db36561aef
Mahesh <70122194+mahesh1105@users.noreply.github.com>
2024-02-28 17:47:26 +0000

    DataTypes Commit

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; // Treat all JS code as newer version
+
+// To Generate the pop up window on the browser
+// alert(3 + 3) // we are using Node JS not browser
+
+/*
+console.log(3
+    +
+    3) // Code reusability should be high
+*/
+
+// Primitive DataTypes
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+let num = 20
+let n = BigInt(200000)
+let str = "Mahesh"
+let isReady = true
+let k = null
+let val
+
+console.log(typeof num)
+console.log(typeof n)
+console.log(typeof str)
+console.log(typeof isReady)
+console.log(typeof k)
+console.log(typeof val)
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+// Objects - Later talk about it
