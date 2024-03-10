c35b7dfc93f927caa2e9d717aeb2c39238f602fb
Mahesh <70122194+mahesh1105@users.noreply.github.com>
2024-03-10 18:28:24 +0000

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -79,5 +79,30 @@ let booleanValue2 = Boolean(val2);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************** Operations ***************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Mahesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log(1 + "2"); // 12
+console.log("1" + 2); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
