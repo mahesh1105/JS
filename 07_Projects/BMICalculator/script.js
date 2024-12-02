9e76fd9a1cdf1041224fc682e5f721cfa7c0964b
mahesh1105 <maheshsaini1105@gmail.com>
2024-12-02 12:23:48 +0530

    BMICalculator Project

--- 07_Projects/BMICalculator/script.js
+++ 07_Projects/BMICalculator/script.js
@@ -1,34 +1,3 @@
-# DOM Projects
-
-## Project Link
-[Click Here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution Code
-
-## Project 1
-
-``` JavaScript
-const buttons = document.querySelectorAll('.button')
-const body = document.querySelector('body')
-
-buttons.forEach((button) => {
-    console.log(button)
-
-    button.addEventListener('click', (e) => {
-        // Print the event
-        console.log(e)
-        // Print the target from which the event coming
-        console.log(e.target)
-
-        // Change the Background Color by Id
-        body.style.backgroundColor = e.target.id
-    })
-})
-```
-
-## Project 2
-
-``` JavaScript
 const form = document.querySelector('form')
 
 // This usecase will give you empty height and weight
@@ -60,4 +29,3 @@ form.addEventListener('submit', (e) => {
         }
     }
 })
-```
