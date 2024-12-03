3d622d6f6e8d666804a7c98c0517ebe41f684a13
mahesh1105 <maheshsaini1105@gmail.com>
2024-12-03 20:59:23 +0530

    Keyboard Key Project

---
+++ 07_Projects/5-KeyboardKey/script.js
@@ -0,0 +1,23 @@
+// Target - whenever you press any key, it should be displayed on window
+// i.e. which key you pressed, keyCode and keyName
+
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class="color">
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>KeyCode</th>
+                <th>KeyName</th>
+            </tr>
+            <tr>
+                <td>${e.key === " " ? 'Space' : e.key}</td>
+                <td>${e.keyCode}</td>
+                <td>${e.code}</td>
+            </tr>
+        </table>
+    </div>
+    `
+})
