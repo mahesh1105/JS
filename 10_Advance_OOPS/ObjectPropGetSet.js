abb718ae0704ab011759a61395b99c4ed4305961
mahesh1105 <maheshsaini1105@gmail.com>
2024-12-06 21:36:13 +0530

    Object Properties Setters and Getters

--- 10_Advance_OOPS/ObjectPropGetSet.js
+++ 10_Advance_OOPS/ObjectPropGetSet.js
@@ -15,3 +15,27 @@ const user = {
 // To Create the separate instance of the user use Object.create()
 const us = Object.create(user)
 console.log(us.email);
+
+// Creating getters and setters with email and password
+// Note: Property name must differ from setters and getters name
+const user2 = {
+    _email: "mahesh@ai.com",
+    _password: "abcc",
+
+    get email() {
+        return `Email: ${this._email}`
+    },
+    set email(email) {
+        this._email = email
+    },
+    get password() {
+        return `Password: ${this._password}`
+    },
+    set password(password) {
+        this._password = password
+    }
+}
+
+// To Create the separate instance of the user use Object.create()
+const us2 = Object.create(user2)
+console.log(us2._email);
